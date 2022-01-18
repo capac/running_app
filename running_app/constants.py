c0294f6573db946bfbab299ad5c8ef86543c3ebf
capac <capac@users.noreply.github.com>
2022-01-18 19:02:06 +0000

    Added constants file with values of variables

---
+++ running_app/constants.py
@@ -0,0 +1,11 @@
+import enum
+
+
+class FieldTypes(enum.Enum):
+    iso_date_string = 1
+    time_string = 2
+    decimal = 3
+    boolean = 4
+    # integer = 5
+    # string = 6
+    # string_list = 7
