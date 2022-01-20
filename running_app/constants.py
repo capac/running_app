498191828dde9471f9e5fb9761df345aff708ca2
capac <capac@users.noreply.github.com>
2022-01-20 15:59:06 +0000

    Added string, string_list and integer fields

--- running_app/constants.py
+++ running_app/constants.py
@@ -5,7 +5,7 @@ class FieldTypes(enum.Enum):
     iso_date_string = 1
     time_string = 2
     decimal = 3
-    boolean = 4
-    # integer = 5
-    # string = 6
-    # string_list = 7
+    string = 4
+    boolean = 5
+    integer = 6
+    string_list = 7
