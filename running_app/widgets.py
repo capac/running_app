556094ff092a61afd74b7dd1d082b227999adadc
capac <capac@users.noreply.github.com>
2022-01-22 10:30:12 +0000

    Added DateEntry class

--- running_app/widgets.py
+++ running_app/widgets.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from datetime import datetime
 from decimal import Decimal, InvalidOperation
 from .constants import FieldTypes as FT
 
@@ -81,6 +82,34 @@ class ValidatedMixin:
         valid = self._validate('', '', '', 'focusout', '', '')
         if not valid:
             self._focusout_invalid(event='focusout')
+        return valid
+
+
+class DateEntry(ValidatedMixin, ttk.Entry):
+
+    def _key_validate(self, action, index, char, **kwargs):
+        valid = True
+
+        if action == '0':
+            valid = True
+        elif index in ('0', '1', '2', '3', '5', '6', '8', '9'):
+            valid = char.isdigit()
+        elif index in ('4', '7'):
+            valid = char == '-'
+        else:
+            valid = False
+        return valid
+
+    def _focusout_validate(self, event):
+        valid = True
+        if not self.get():
+            self.error.set('A value is required')
+            valid = False
+        try:
+            datetime.strptime(self.get(), '%Y-%m-%d')
+        except ValueError:
+            self.error.set('Invalid date')
+            valid = False
         return valid
 
 
