467d9459c1ca4ccfb5c8e64c04af416f9f3897b1
capac <capac@users.noreply.github.com>
2022-01-28 09:12:44 +0000

    Added 'RecordList' class

--- running_app/views.py
+++ running_app/views.py
@@ -45,3 +45,100 @@ class DataRecordForm(tk.Frame):
                                          input_var=self.callbacks['on_remove'])
         self.removebutton.grid(row=0, column=1, padx=10, pady=(10, 0))
         command_section.grid(row=2, column=0, sticky=(tk.W + tk.E))
+
+
+class RecordList(tk.Frame):
+    '''Display records in the database'''
+
+    column_defs = {
+        '#0': {'label': 'Row', 'anchor': tk.W},
+        'Date': {'label': 'Date', 'width': 120},
+        'Time': {'label': 'Time', 'width': 120},
+        'Distance': {'label': 'Distance', 'width': 80},
+        'Location': {'label': 'Location', 'width': 200},
+    }
+    default_width = 100
+    default_minwidth = 20
+    default_anchor = tk.W
+
+    def __init__(self, parent, callbacks,
+                 inserted, updated,
+                 *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self.callbacks = callbacks
+        self.inserted = inserted
+        self.updated = updated
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+
+        # create treeview
+        self.treeview = ttk.Treeview(self, columns=list(self.column_defs.keys())[1:],
+                                     selectmode='browse')
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+        self.treeview.grid(row=0, column=0, sticky='NSEW')
+
+        # hide first column
+        self.treeview.config(show='headings')
+
+        # configure scrollbar for the treeview
+        self.scrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL,
+                                       command=self.treeview.yview)
+        self.treeview.configure(yscrollcommand=self.scrollbar.set)
+        self.treeview.grid(row=0, column=0, sticky='NSEW')
+        self.scrollbar.grid(row=0, column=1, sticky='NSEW')
+
+        # configure treeview columns
+        for name, definition in self.column_defs.items():
+            label = definition.get('label', '')
+            anchor = definition.get('anchor', self.default_anchor)
+            minwidth = definition.get('minwidth', self.default_minwidth)
+            width = definition.get('width', self.default_width)
+            stretch = definition.get('stretch', False)
+            self.treeview.heading(name, text=label, anchor=anchor)
+            self.treeview.column(name, anchor=anchor, minwidth=minwidth,
+                                 width=width, stretch=stretch)
+
+        # configure row tags
+        self.treeview.tag_configure('inserted_record', background='lightgreen')
+        self.treeview.tag_configure('updated_record', background='deepskyblue')
+
+        # bind selection
+        self.treeview.bind('<<TreeviewSelect>>', self.on_open_record)
+
+    def on_open_record(self, *args):
+        try:
+            selected_id = self.treeview.selection()[0]
+            self.callbacks['on_open_record'](selected_id.split('|')[0])
+        # quick fix when window loses focus and no line is selected,
+        # a better fix is to find a way to keep the line selected
+        except IndexError:
+            pass
+
+    def populate(self, rows):
+        '''Clear the treeview and write the supplied data rows to it'''
+
+        for row in self.treeview.get_children():
+            self.treeview.delete(row)
+
+        valuekeys = list(self.column_defs.keys())[1:]
+        for rowdata in rows:
+            rowkey = (str(rowdata['Date']), str(rowdata['Time']),
+                      str(rowdata['Distance']), str(rowdata['Location']),)
+            values = [rowdata[key] for key in valuekeys]
+            if self.inserted and rowkey in self.inserted:
+                tag = 'inserted_record'
+            elif self.updated and rowkey in self.updated:
+                tag = 'updated_record'
+            else:
+                tag = ''
+            stringkey = '{}|{}|{}|{}'.format(*rowkey)
+            self.treeview.insert('', 'end', iid=stringkey, text=stringkey,
+                                 values=values, tag=tag)
+
+        # selects automatically the first row, to make selections keyboard-friendly
+        if len(rows) > 0:
+            firstrow = self.treeview.identify_row(0)
+            self.treeview.focus_set()
+            self.treeview.selection_set(firstrow)
+            self.treeview.focus(firstrow)
