11f2be4ad573701733b3bf411876926e4c461b28
capac <capac@users.noreply.github.com>
2022-02-05 09:53:21 +0000

    Added 'SettingsModel' class

--- running_app/models.py
+++ running_app/models.py
@@ -1,6 +1,7 @@
 import sqlite3
 import csv
 import os
+import json
 from datetime import timedelta
 from .constants import FieldTypes as FT
 
@@ -165,3 +166,53 @@ class CSVModel:
             csvwriter.writeheader()
             for row in rows:
                 csvwriter.writerow(row)
+
+
+class SettingsModel:
+    '''A model for saving settings'''
+
+    variables = {
+        # ('aqua', 'clam', 'alt', 'default', 'classic')
+        'theme': {'type': 'str', 'value': 'aqua'},
+        'db_name': {'type': 'str', 'value': 'running.db'},
+    }
+
+    def __init__(self, filename='settings.json', path='~'):
+        # determine the file path
+        self.filepath = os.path.join(os.path.expanduser(path), filename)
+
+        # load in saved values
+        self.load()
+
+    def set(self, key, value):
+        '''Set a variable value'''
+
+        if (
+            key in self.variables and type(value).__name__ == self.variables[key]['type']
+        ):
+            self.variables[key]['value'] = value
+        else:
+            raise ValueError('Bad key or wrong variable type')
+
+    def save(self, settings=None):
+        '''Save the current settings to the file'''
+
+        json_string = json.dumps(self.variables)
+        with open(self.filepath, 'w') as fh:
+            fh.write(json_string)
+
+    def load(self):
+        '''Load the settings from the file'''
+
+        # if the file doesn't exist, return
+        if not os.path.exists(self.filepath):
+            return
+        # open the file and read the raw values
+        with open(self.filepath, 'r') as fh:
+            raw_values = json.loads(fh.read())
+        # don't implicitly trust the raw values,
+        # but only get known keys
+        for key in self.variables:
+            if key in raw_values and 'value' in raw_values[key]:
+                raw_value = raw_values[key]['value']
+                self.variables[key]['value'] = raw_value
