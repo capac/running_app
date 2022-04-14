4c37d75e0ed65228d8b9f452e4f6851744804163
capac <capac@users.noreply.github.com>
2022-04-14 15:56:28 +0100

    Added 'Add marathon plan' file menu entry

--- running_app/views.py
+++ running_app/views.py
@@ -32,13 +32,8 @@ class MainMenu(tk.Menu):
         file_menu.add_separator()
         file_menu.add_command(
             # 8230: ASCII value for horizontal ellipsis
-            label='Show level-1 novice marathon plan'+chr(8230),
-            command=callbacks['show_novice_1_plan']
-            )
-        file_menu.add_command(
-            # 8230: ASCII value for horizontal ellipsis
-            label='Show level-2 novice marathon plan'+chr(8230),
-            command=callbacks['show_novice_2_plan']
+            label='Add marathon plan'+chr(8230),
+            command=callbacks['add_plan']
             )
         self.add_cascade(label='File', menu=file_menu)
 
