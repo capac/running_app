afce9451a51482c0ea766df303075c7fc5ada4a8
capac <capac@users.noreply.github.com>
2022-06-14 12:35:39 +0100

    Added 'iso_pace_string' field type

--- running_app/constants.py
+++ running_app/constants.py
@@ -4,8 +4,9 @@ import enum
 class FieldTypes(enum.Enum):
     iso_date_string = 1
     iso_time_string = 2
-    decimal = 3
-    integer = 4
-    string = 5
-    string_list = 6
-    boolean = 7
+    iso_pace_string = 3
+    decimal = 4
+    integer = 5
+    string = 6
+    string_list = 7
+    boolean = 8
