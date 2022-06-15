cf72d0bb814d656820d419f301dc83ca17d9f4f7
capac <capac@users.noreply.github.com>
2022-06-15 17:35:58 +0100

    Added 'iso_duration_string' field type

--- running_app/constants.py
+++ running_app/constants.py
@@ -5,8 +5,9 @@ class FieldTypes(enum.Enum):
     iso_date_string = 1
     iso_time_string = 2
     iso_pace_string = 3
-    decimal = 4
-    integer = 5
-    string = 6
-    string_list = 7
-    boolean = 8
+    iso_duration_string = 4
+    decimal = 5
+    integer = 6
+    string = 7
+    string_list = 8
+    boolean = 9
