e8323d1dbd8353324f9b69c2522bf5574afa4386
capac <capac@users.noreply.github.com>
2022-06-16 08:16:39 +0100

    Set dpi=75 in 'BarChartWidget' class

--- running_app/widgets.py
+++ running_app/widgets.py
@@ -443,7 +443,7 @@ class BarChartWidget(tk.Frame):
 
     def __init__(self, parent, x_label, y_label, title, figsize=(12, 3), *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
-        self.figure = Figure(figsize=figsize, dpi=80, layout='tight')
+        self.figure = Figure(figsize=figsize, dpi=75, layout='tight')
         self.canvas = FigureCanvasTkAgg(self.figure, master=self)
         self.canvas.get_tk_widget().pack(fill='both', expand=True)
         # axes
