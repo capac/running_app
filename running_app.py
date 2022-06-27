22cace6c819d28fb1ccb354831daae881eccdcb8
capac <capac@users.noreply.github.com>
2022-06-27 11:06:34 +0100

    Added stack trace error logging

--- running_app.py
+++ running_app.py
@@ -1,4 +1,12 @@
 from running_app.application import Application
+import logging
 
-app = Application()
-app.mainloop()
+# initialize the log settings
+# https://code.tutsplus.com/tutorials/error-handling-logging-in-python--cms-27932
+logging.basicConfig(filename='app.log', level=logging.INFO)
+
+try:
+    app = Application()
+    app.mainloop()
+except IOError as e:
+    logging.exception(str(e))
