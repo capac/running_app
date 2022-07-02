49bc3b0c4630438e1096961ac2e8c75e21f0ae83
capac <capac@users.noreply.github.com>
2022-07-02 09:23:27 +0100

    Added 'iso_date_list' to 'Search date'

--- running_app/models.py
+++ running_app/models.py
@@ -20,7 +20,7 @@ class SQLModel:
                    'values': ['1', '3', '6']},
         # data fields for search form
         'Search duration': {'req': True, 'type': FT.iso_duration_string},
-        'Search date': {'req': True, 'type': FT.string_list},
+        'Search date': {'req': True, 'type': FT.iso_date_list},
         'Search pace': {'req': True, 'type': FT.iso_pace_string},
         }
 
